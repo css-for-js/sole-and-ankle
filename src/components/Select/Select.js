b6fa03cede48d1ed912818386b26ff9be33788de
Aleksandra Baryshnikova <lexie0428@yandex.ru>
2021-10-28 21:04:40 +0300

    stylize shell

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -32,7 +32,10 @@ const Select = ({ label, value, children, ...delegated }) => {
   );
 };
 
-const Wrapper = styled.label``;
+const Wrapper = styled.label`
+  display: flex;
+  align-items: baseline;
+`;
 
 const VisibleLabel = styled.span`
   color: ${COLORS.gray[700]};
