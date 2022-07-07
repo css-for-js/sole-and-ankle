956f03186fb063c953ca4b155a1eeefce856ca42
Luis <mendoza386@gmail.com>
2022-07-07 23:11:15 +0200

    header done, needed to watch solution for properly horizontal centering of the nav component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <Side>
+          <Logo />
+        </Side>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,17 +23,30 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Side />
       </MainHeader>
     </header>
   );
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  height: 72px;
+  display: flex; 
+  align-items: baseline; 
+  justify-content: center;
+  padding: 18px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
 `;
 
-const Nav = styled.nav``;
+const Side = styled.div`
+  flex: 1; 
+`
+
+const Nav = styled.nav`
+  display: flex; 
+  gap: 48px;
+  margin: 0 48px; 
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
