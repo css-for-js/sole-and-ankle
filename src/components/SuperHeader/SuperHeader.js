0704fdbe5ab6b068e5ca436a17f4a9ecfe63cc0e
AlaaAlajmi <94835266+AlaaAlajmi@users.noreply.github.com>
2022-07-21 13:20:25 +0300

    Exercise 1: Superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,16 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  height: 40px;
+  align-items: center;
+  padding: 0 32px;
+  gap: 24px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
